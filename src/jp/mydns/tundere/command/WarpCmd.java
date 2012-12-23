c926a76dd2472b432ebb4c95268e6d3c3982c221
tsuttsu305 <tsuttsu305@daiwa-hotcom.com>
2012-12-23 16:42:51 +0900

    Warp Cmd update

--- src/jp/mydns/tundere/command/WarpCmd.java
+++ src/jp/mydns/tundere/command/WarpCmd.java
@@ -6,14 +6,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+/**
+ * 
+ * @author tsuttsu305
+ *
+ */
 public class WarpCmd {
 	/*こんすとらくたぁ～ここから*/
 	private TundereAdmin plugin;
 	private CommandSender sender;
 	private String[] args;
-
+	
 	public WarpCmd(TundereAdmin plugin, CommandSender sender,
 			Command cmd, String label, String[] args) {
 		this.plugin = plugin;
@@ -21,7 +27,7 @@ public class WarpCmd {
 		this.args = args;
 	}
 	/*こんすとらくたぁ～ここまで*/
-
+	
 	/*PermissionNodeここから*/
 	private final String rootPerm = "tundere.warp";
 	private final String setPerm = "set";
@@ -30,7 +36,7 @@ public class WarpCmd {
 	private final String rmPerm = "remove";
 	private final String reloadPrem = "reload";
 	/*PermissionNodeここまで*/
-
+	
 	public void cmdRun(){
 		if (args.length < 1){
 			sender.sendMessage(ChatColor.GOLD + "[TundereAdnim] /warp [set|to|list|remove]");
@@ -52,26 +58,34 @@ public class WarpCmd {
 			
 		}else if (args[0].equalsIgnoreCase("to")){
 			if (chkPerm(rootPerm  + toPerm) == false) return; 
-			
+			if (sender instanceof Player){
+				if (args.length == 2){
+					doWarp(args[1], (Player)sender);
+				}else{
+					((Player)sender).sendMessage(ChatColor.RED + "[TundereAdmin] 引数が異常です!");
+				}
+			}else{
+				sender.sendMessage("[TundereAdmin] このコマンドはPlayer専用です。");
+			}
 			
 		}else if(args[0].equalsIgnoreCase("list")){
 			if (chkPerm(rootPerm  + listPerm) == false) return; 
-
+			
 			
 		}else if(args[0].equalsIgnoreCase("remove")){
 			if (chkPerm(rootPerm  + rmPerm) == false) return; 
-
+			
 			
 		}else if(args[0].equalsIgnoreCase("reload")){
 			if (chkPerm(rootPerm  + reloadPrem) == false) return; 
 			plugin.getWarpLocConfig().loadConfig();
 			sender.sendMessage(ChatColor.GREEN + "[TundereAdmin] Warp Config Reload SUCCESS!");
 		}else{
-
+			
 		}
 	}
-
-
+	
+	
 	/**
 	 * 権限ないよm9用処理
 	 */
@@ -80,11 +94,11 @@ public class WarpCmd {
 			return true;
 		}
 		if (((Player)sender).hasPermission(perm)) return true;
-
+		
 		sender.sendMessage(ChatColor.RED + "[TundereAdmin] 権限がありません。");
 		return false;
 	}
-
+	
 	/**
 	 * ワープポイント設定
 	 * @param pl :Player
@@ -93,34 +107,67 @@ public class WarpCmd {
 	public void setWarp(Player pl, String locName){
 		Location here = pl.getLocation();
 		int x, y, z;
+		String worldName = here.getWorld().getName();
 		x = here.getBlockX();
 		y = here.getBlockY();
 		z = here.getBlockZ();
-		plugin.getWarpLocConfig().getConfig().set(locName + ".x", x);
-		plugin.getWarpLocConfig().getConfig().set(locName + ".y", y);
-		plugin.getWarpLocConfig().getConfig().set(locName + ".z", z);
+		
+		getWarpConfig().set(locName + ".x", x);
+		getWarpConfig().set(locName + ".y", y);
+		getWarpConfig().set(locName + ".z", z);
+		getWarpConfig().set(locName + ".world", worldName);
 		plugin.getWarpLocConfig().saveConfig();
 		
 		pl.sendMessage(ChatColor.GREEN + "[TundereAdmin] 保存しました! " + locName + ": " + x + ", " + y + ", " + z);
 	}
 	
-	public void doWarp(String locKey){
+	
+	/**
+	 * Playerをワープさせる
+	 * @param locKey :yml key
+	 * @param player :Player
+	 */
+	public void doWarp(String locKey, Player player){
+		//locKeyが登録されているか判定
+		if (!getWarpConfig().isConfigurationSection(locKey)){
+			player.sendMessage(ChatColor.RED + "[TundereAdmin] その場所は登録されていないようです。");
+			return;
+		}
+		//設定値がおかしい時
+		if (!getWarpConfig().isInt(locKey + ".x") || 
+				!getWarpConfig().isInt(locKey + ".y") || !getWarpConfig().isInt(locKey + ".z") || 
+				!getWarpConfig().isString(locKey + ".world")){
+			player.sendMessage(ChatColor.RED + "[TundereAdmin] 設定ファイルに異常があります。");
+			return;
+		}
+		
+		
+		//設定ファイルから取得
 		int x, y, z;
-		x = plugin.getWarpLocConfig().getConfig().getInt(locKey + ".x");
-		y = plugin.getWarpLocConfig().getConfig().getInt(locKey + ".y");
-		z = plugin.getWarpLocConfig().getConfig().getInt(locKey + ".z");
-		//TODO: なんかいいFont探し
+		String worldName = getWarpConfig().getString(locKey + ".world");
+		x = getWarpConfig().getInt(locKey + ".x");
+		y = getWarpConfig().getInt(locKey + ".y");
+		z = getWarpConfig().getInt(locKey + ".z");
+		
+		//Worldが存在するか判定
+		if (plugin.getServer().getWorld(worldName) == null){
+			player.sendMessage(ChatColor.RED + "[TundereAdmin] World(" + worldName +  ")が存在しません!");
+		}
+		
+		//移動先Location生成
+		Location toLoc = new Location(plugin.getServer().getWorld(worldName), x, y, z);
+		
+		//移動
+		player.teleport(toLoc);
+		player.sendMessage(ChatColor.GREEN + "[TundereAdmin] " + locKey + " にワープしました!");
+	}
+	
+	
+	/**
+	 * Warp地点のConfigファイル呼び出し用
+	 * @return WarpLocListConfig
+	 */
+	private FileConfiguration getWarpConfig(){
+		return plugin.getWarpLocConfig().getConfig();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
